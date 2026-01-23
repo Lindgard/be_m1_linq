54cbc2c15b0c0180aff58f9da74354655fe6b00d
ALP <a.b.lindgard@gmail.com>
2026-01-23 13:52:41 +0100

    began the datahandling in a separate file from program.cs

---
+++ Classes/DataHandler.cs
@@ -0,0 +1,28 @@
+namespace be_m1_linq.Classes;
+
+public class DataHandler
+{
+    //*grab data from csv-file in library-folder
+    private readonly string _path;
+
+    //* Metode så man faktisk kan lage if-statements osv...
+    public DataHandler(string path)
+    {
+        _path = path;
+
+        if (!File.Exists(_path))
+        {
+            throw new FileNotFoundException($"CSV file not found at: {_path}");
+        }
+
+    }
+
+
+
+    //TODO read file and make available for other methods
+
+
+    // lower amount of data to print in output
+    // Search for items containing the user-input
+    // use LINQ in searches and sorting
+}
